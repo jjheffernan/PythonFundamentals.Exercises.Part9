7a0fd5d08dd805d4a93f21d89d04777a5c00a88d
jjheffernan <jjheffernan99@gmail.com>
2022-07-24 18:41:23 -0400

    configured base test case structure. Need to further adapt.

--- test_json_helper.py
+++ test_json_helper.py
@@ -11,7 +11,7 @@ Use the below fileReader Test class to work develop framework for
 class FileReaderTest(unittest.TestCase):
 
     @patch('sys.stdout', new_callable=StringIO)
-    def test_reckless_file_reader_happy_path_(self, mock_stdout):
+    def test_read_json(self, mock_stdout):
         test_cases = [
             (
                 "my_awesome_file.txt",
@@ -26,7 +26,7 @@ class FileReaderTest(unittest.TestCase):
                 actual = mock_stdout.getvalue()
                 self.assertEqual(expected, actual)
 
-    def test_reckless_file_reader_exception(self):
+    def test_read_all_json_files(self):
         test_cases = [
             ("locked_out_file.txt", PermissionError),
             ("file_that_does_not_exist.txt", FileNotFoundError)
@@ -37,20 +37,20 @@ class FileReaderTest(unittest.TestCase):
                     file_reader.reckless_file_reader(file_path)
 
     @unittest.skip("Useless test. This exists simply to trigger and demo the function under test.")
-    def test_quick_way_to_get_fired(self):
+    def test_write_pickle(self):
         with self.assertRaises(PermissionError):
             file_reader.quick_way_to_get_fired("locked_out_file.txt")
 
-    def test_single_exception_handling_reader_handled(self):
+    def test_is_dir(self):
         with self.assertLogs() as cm:
             file_reader.single_exception_handling_reader("file_that_does_not_exist.txt")
             self.assertIn("No such file or directory: 'file_that_does_not_exist.txt'", cm.output[0])
 
-    def test_single_exception_handling_reader_unhandled(self):
+    def test_is_json(self):
         with self.assertRaises(PermissionError):
             file_reader.single_exception_handling_reader("locked_out_file.txt")
 
-    def test_multiple_exception_handling_reader(self):
+    def test_load_pickle(self):
         test_cases = [
             (
                 "file_that_does_not_exist.txt",
@@ -66,51 +66,3 @@ class FileReaderTest(unittest.TestCase):
                 with self.assertLogs() as cm:
                     file_reader.multiple_exception_handling_reader(file_path)
                     self.assertIn(message, cm.output[0])
-
-    @patch('sys.stdout', new_callable=StringIO)
-    def test_base_class_exception_handling_reader(self, mock_stdout):
-        test_cases = [
-            "locked_out_file.txt",
-            "file_that_does_not_exist.txt"
-        ]
-        for file_path in test_cases:
-            with self.subTest(f"{file_path}"):
-                file_reader.base_class_exception_handling_reader(file_path)
-                self.assertIn(
-                    "Error opening the file. Please ensure the file exists and has appropriate permissions.",
-                    mock_stdout.getvalue()
-                )
-
-    @patch('sys.stdout', new_callable=StringIO)
-    def test_tuple_exception_handling_reader(self, mock_stdout):
-        test_cases = [
-            "locked_out_file.txt",
-            "file_that_does_not_exist.txt"
-        ]
-        for file_path in test_cases:
-            with self.subTest(f"{file_path}"):
-                file_reader.tuple_exception_handling_reader(file_path)
-                self.assertIn(
-                    "Error opening the file. Please ensure the file exists and has appropriate permissions.",
-                    mock_stdout.getvalue()
-                )
-
-    @patch('sys.stdout', new_callable=StringIO)
-    def test_better_file_reader_known_exceptions(self, mock_stdout):
-        test_cases = [
-            "locked_out_file.txt",
-            "file_that_does_not_exist.txt"
-        ]
-        for file_path in test_cases:
-            with self.subTest(f"{file_path}"):
-                file_reader.better_file_reader(file_path)
-                self.assertIn(
-                    "Error opening the file. Please ensure the file exists and has appropriate permissions.",
-                    mock_stdout.getvalue()
-                )
-
-    def test_better_file_reader_known_happy_path(self):
-        with patch('file_reader.process_file') as cm:
-            file_reader.better_file_reader("my_awesome_file.txt")
-        cm.assert_called_once()
-
